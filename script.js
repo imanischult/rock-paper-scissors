9d6b7941e263a6800b2b5eac47c59636d016657d
Imani Schulterbrandt <imani.vicente@outlook.com>
2020-04-24 22:00:47 -0400

    added restart and goodgame functions

--- script.js
+++ script.js
@@ -17,30 +17,59 @@ var computerPick = function () {
 
 var pointCheck = function (user, computer) {
     if (user < 10 && computer < 10) {
-        console.log("Pick again!");
+        document.getElementById("choose").innerHTML = "Pick again!";
     } else if (computer === 10) {
-        console.log("You lost the game!");
+        document.getElementById("win-lose").innerHTML = "You lost the game!";
+        displayRestart();
     } else if (user === 10) {
-        console.log("You won the game!");
+        document.getElementById("win-lose").innerHTML = "You won the game!";
+        displayRestart()
     };
 };
 
+var restart = function () {
+    hideRestart();
+    document.getElementById("choose").innerHTML = "";
+    document.getElementById("user-input").style.display = "none";
+    document.getElementById("computer-input").style.display = "none";
+    userScore = 0;
+    compScore = 0;
+}
+
+var goodGame = function () {
+    hideRestart();
+    document.getElementById("choose").innerHTML = "";
+    document.getElementById("user-weapon").style.display = "none";
+    document.getElementById("computer-weapon").style.display = "none";
+    document.getElementById("win-lose").innerHTML = "Good Game!"
+    document.getElementById("user-final").innerHTML = "Your Final Score: " + userScore;
+    document.getElementById("comp-final").innerHTML = "Computer's Final Score: " + compScore;
+}
+
 var userWonRound = function () {
-    console.log("You won this round!")
+    document.getElementById("win-lose").innerHTML = "You won this round!"
     document.querySelector("#user-points").innerHTML = userScore
     document.querySelector("#computer-points").innerHTML = compScore
 };
 
 var compWonRound = function () {
-    console.log("You lost this round")
+    document.getElementById("win-lose").innerHTML = "You lost this round!"
     document.querySelector("#user-points").innerHTML = userScore
     document.querySelector("#computer-points").innerHTML = compScore
 };
 
-var gameLogic = function (compChoice) {
+var hideRestart = function () {
+    document.getElementById("restart").style.display = "none"
+}
 
+var displayRestart = function () {
+    document.getElementById("restart").style.display = "block"
+    document.getElementById("")
+}
+
+var gameLogic = function (compChoice) {
     if (compChoice === "ROCK" && rock.checked || compChoice === "PAPER" && paper.checked || compChoice === "SCISSORS" && scissors.checked) {
-        alert("It's a tie! Try again")
+        document.getElementById("win-lose").innerHTML = "It's a tie!"
     } else if (compChoice === "ROCK" && paper.checked) {
         userScore++;
         userWonRound();
@@ -67,6 +96,8 @@ var gameLogic = function (compChoice) {
         pointCheck(userScore, compScore);
     };
 }
+
+hideRestart();
 
 submitEl.addEventListener("click", function (event) {
     event.preventDefault();
